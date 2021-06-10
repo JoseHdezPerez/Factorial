41afbb3dbfddff6a6347328661aa4e283462fe95
JoseHdezPerez <josehernandezbp@gmail.com>
2021-06-10 18:57:43 +0200

    Commit changes

--- Factorial/src/factorial/Factorial.java
+++ Factorial/src/factorial/Factorial.java
@@ -14,6 +14,7 @@ public class Factorial {
         if(n<0){
             throw new IllegalArgumentException("Número " +n+ " no puede ser < que 0");
         }
+		//HOLA QUE TAL
         int fact=1;
         for(int i=2 ;i<=n ; i++)
             fact*=i;
